38f4c33e2f729891a47682500bb9f8efbe006327
Omid Memarrast <omid.memarrast@gmail.com>
2017-02-05 19:51:16 -0600

    Changed input files to read Nilanja's test case.

--- src/IOUtils.java
+++ src/IOUtils.java
@@ -14,8 +14,8 @@ import java.util.regex.Pattern;
 public class IOUtils {
 
     // Relative path of default files
-    public static final String INPUT_TRANSACTION_PATH = "data\\input-data-anna.txt";
-    public static final String INPUT_PARAMETERS_PATH = "data\\parameter-file-anna.txt";
+    public static final String INPUT_TRANSACTION_PATH = "data\\input-data-nilanjana.txt";
+    public static final String INPUT_PARAMETERS_PATH = "data\\parameter-file-nilanjana.txt";
 
     public static final String OUTPUT_RESULT = "data\\output.txt";
 
